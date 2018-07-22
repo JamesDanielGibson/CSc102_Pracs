a4e4fa1d1558a437bfdf52fb42b73bf620920714
james <james@DESKTOP-8NDM0QE>
2018-07-22 16:28:16 +0200

    drips2

--- Prac01/Drips/Program.cs
+++ Prac01/Drips/Program.cs
@@ -12,11 +12,20 @@ namespace Drips
         {
             Console.Write("how often do you hear a drop <in seconds>? ");
             string strDrip = Console.ReadLine();
-            double dripTime = Convert.ToDouble(strDrip);
-            Console.WriteLine("This Wastes " + 3600 / dripTime * 0.00025+" litres every hour");
-            Console.WriteLine("This Wastes " + 86400 / dripTime * 0.00025+" litres every day");
-            Console.WriteLine("This Wastes " + 604800 / dripTime * 0.00025+" litres every week");
-            Console.ReadLine();
+            Int32 number;
+            bool b = Int32.TryParse(strDrip, out number);
+            if (b) 
+            {
+                Int32 dripTime = Convert.ToInt32(strDrip);
+                Console.WriteLine("This Wastes " + (Convert.ToInt16(3600 / dripTime * 0.00025) / 0.25) + " cups of water every hour");
+                Console.WriteLine("This Wastes " + (Convert.ToInt16(86400 / dripTime * 0.00025) / 0.25) + " cups of water every day");
+                Console.WriteLine("This Wastes " + (Convert.ToInt16(604800 / dripTime * 0.00025) / 0.25) + " cups of water every week");
+            }
+            else
+            {
+                Console.WriteLine("That was not a number.");
+            }
+                Console.ReadLine();
         }
     }
 }
